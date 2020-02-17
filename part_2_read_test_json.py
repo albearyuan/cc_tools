5411805bfcf87e0bd82fa2db9372c47229b0b48a
Albert Yuan <albear@Alberts-MBP.fios-router.home>
2020-02-17 17:56:56 -0500

    phase 2

--- part_2_read_test_json.py
+++ part_2_read_test_json.py
@@ -14,6 +14,13 @@ def make_game_library_from_json( json_data ):
         #  platform (which requires reading name and launch_year)
         #Add that Game object to the game_library
     ### End Add Code Here ###
+    
+    json_lib = json_data['game_library']
+    
+    for json_game in json_lib:
+        platform = test_data.Platform(json_game['platform']['name'], json_game['platform']['launch_year'])
+        game = test_data.Game(json_game['title'], platform, json_game['year'])
+        game_library.add_game(game)
 
     return game_library
 
@@ -27,3 +34,8 @@ input_json_file = "data/test_data.json"
 #Use make_game_library_from_json(json_data) to convert the data to GameLibrary data
 #Print out the resulting GameLibrary data using print()
 ### End Add Code Here ###
+
+with open(input_json_file) as json_file:
+    data = json.load(json_file)
+    game_library = make_game_library_from_json(data)
+    print(game_library)
